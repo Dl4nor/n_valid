a5dc8f7989f8b31364258924be5e5e55a21abb9f
Ronald VS <ronald.silva10@fatec.sp.gov.br>
2024-09-18 20:30:23 -0300

    Add TextWithBorder Class && crash-a-like font

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 import 'app_controller.dart';
@@ -18,17 +17,12 @@ class HomePageState extends State<HomePage>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: const TextWithBorder(text: 'N. Valid', font: 'crash-a-like', size: 40, color: Colors.orange),
         backgroundColor: Colors.green,
       ),
       
       body: Center(
-        child: Switch(
-          value: AppController.instance.isDarkTheme, 
-          onChanged: (value) {
-            AppController.instance.changeTheme();
-          },
-        ),
+        child: CustomSwitch()
       ),
 
       floatingActionButton: FloatingActionButton(
@@ -43,4 +37,56 @@ class HomePageState extends State<HomePage>{
    
   }
 
+}
+
+class CustomSwitch extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+            value: AppController.instance.isDarkTheme, 
+            onChanged: (value) {
+              AppController.instance.changeTheme();
+            },
+           );
+  }
+}
+
+class TextWithBorder extends StatelessWidget {
+  
+  final String text;
+  final String font;
+  final double size;
+  final Color color;
+  final Color borderColor;
+
+  const TextWithBorder({super.key, required this.text, 
+                                   this.font = '', 
+                                   required this.size, 
+                                   this.color = Colors.black, 
+                                   this.borderColor = Colors.black});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+                  Text(text, 
+                        style: TextStyle(
+                                fontFamily: font, 
+                                fontSize: size,
+                                foreground: Paint()
+                                  ..style = PaintingStyle.stroke
+                                  ..strokeWidth = 2 // Tamanho da borda
+                                  ..color = borderColor, // Cor da borda
+                                )
+                  ),
+                  Text(text, 
+                        style: TextStyle(
+                                  fontFamily: font, 
+                                  fontSize: size,
+                                  color: color
+                                ),
+                  )
+                ]
+    );
+  }
 }
